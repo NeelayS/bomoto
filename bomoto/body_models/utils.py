0edc97aeb06ed396e53d47bf027e2743913c58b7
Giorgio Becherini <giorgio.becherini@tuebingen.mpg.de>
2023-12-15 15:50:06 +0100

    Added utility functions

--- bomoto/body_models/utils.py
+++ bomoto/body_models/utils.py
@@ -2,7 +2,7 @@ from typing import Union
 
 import numpy as np
 import torch
-
+from scipy.spatial.transform import Rotation, Slerp
 from . import BodyModel
 
 betas_params_names = ['betas', 'shape']
@@ -81,9 +81,68 @@ def rotate_points_around_axis(v: np.ndarray, deg: float, axis: Union[int, str]):
     else:
         raise ValueError("axis must be 0 (=x), 1 (=y) or 2 (=z)")
 
+    if len(v.shape) == 1:
+        return (v[None, ...] @ rot_mat.T).squeeze()
     if len(v.shape) == 2:
         return v @ rot_mat.T
     elif len(v.shape) == 3:
         return np.einsum('nvj, ij -> nvi', v, rot_mat)
     else:
         raise ValueError("v must be 2 or 3 dimensional")
+
+
+def rotvec_slerp(initial, target, n_frames_interp=None, interp_coeffs=None):
+    if interp_coeffs is None:
+        interp_coeffs = np.linspace(0, 1, n_frames_interp, dtype=np.float32)[..., None]
+    initial, target = initial.reshape(-1, 3), target.reshape(-1, 3)
+    joint_rots = [np.stack([r_initial, r_target]) for r_initial, r_target in zip(initial, target)]
+    joint_rots = [Rotation.from_rotvec(jr) for jr in joint_rots]
+    slerps = [Slerp([0, 1], jr)(interp_coeffs.squeeze()).as_rotvec() for jr in joint_rots]
+    return np.concatenate(slerps, axis=1)
+
+
+def lerp(initial, target, n_frames_interp=None, interp_coeffs=None):
+    if interp_coeffs is None:
+        interp_coeffs = np.linspace(0, 1, n_frames_interp, dtype=np.float32)[..., None]
+    interp_vector = target - initial
+    transition = initial + interp_coeffs * interp_vector[None, ...]
+    return transition
+
+
+def interpolate_parameters(target_betas: np.ndarray,
+                           target_pose: np.ndarray,
+                           target_trans: np.ndarray,
+                           initial_betas: np.ndarray = None,
+                           initial_pose: np.ndarray = None,
+                           initial_trans: np.ndarray = None,
+                           n_frames_interp: int = 100):
+    """
+    Interpolates SMPL-X parameters using linear interpolation for betas and translation and spherical linear
+    interpolation for pose.
+
+    Args:
+        target_betas:
+        target_pose:
+        target_trans:
+        initial_betas:
+        initial_pose:
+        initial_trans:
+        n_frames_interp:
+
+    Returns:
+        interpolated SMPL-X parameters
+
+    """
+    # interp_coeffs = (np.arange(n_frames_interp).astype(np.float32)[..., None] / n_frames_interp)
+    interp_coeffs = np.linspace(0, 1, n_frames_interp, dtype=np.float32)[..., None]
+
+    initial_betas = target_betas if initial_betas is None else initial_betas
+    initial_pose = target_pose if initial_pose is None else initial_pose
+    initial_trans = target_trans if initial_trans is None else initial_trans
+
+    transition_poses = rotvec_slerp(initial_pose, target_pose, n_frames_interp=None, interp_coeffs=interp_coeffs)
+
+    transition_betas = lerp(initial_betas, target_betas, n_frames_interp=None, interp_coeffs=interp_coeffs)
+    transition_trans = lerp(initial_trans, target_trans, n_frames_interp=None, interp_coeffs=interp_coeffs)
+
+    return transition_betas, transition_poses, transition_trans
